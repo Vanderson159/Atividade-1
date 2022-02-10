8d0aa58ceb660031ad005fdd4b377d74a71766e1
Vanderson Duarte Maciel <78940842+Vanderson159@users.noreply.github.com>
2022-02-09 21:30:47 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+    
+    $ano = "1700";
+
+    function SeculoAno($ano){
+        $tamanho = strlen($ano);
+       // $aux = $tamanho * -1;
+        $doisUltimos = substr($ano, -2); //pegando os dois últimos caracteres
+        
+       if($doisUltimos == 00){
+            $seculo = substr($ano, 0, $tamanho-2); //pegando os dois primeiros caracteres
+       }else{
+            if($tamanho > 3){
+                $seculo = substr($ano, 0, $tamanho-2);
+                $seculo += 1;
+            }else{
+                if($tamanho < 4){
+                    $seculo = substr($ano, 0, 1); //pegando os dois primeiros caracteres
+                    $seculo += 1;
+                }
+            } 
+       }
+
+        echo ("Ano ".$ano." = século ".$seculo);
+    }
+
+    SeculoAno($ano);
+?>
